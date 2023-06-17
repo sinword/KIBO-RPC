3ee6dd2876235923f7eef125774fe2d59ed8589d
MeowOhMeow <abc22681157@gmail.com>
2023-06-18 00:57:51 +0800

    testing for taking snapshots on 2 different angle

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,6 +1,8 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
 
 /**
  * Class meant to handle commands from the Ground Data System and execute them
@@ -8,6 +10,8 @@ import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
  */
 
 public class YourService extends KiboRpcService {
+
+    private static final int LOOP_LIMIT = 5;
 
     @Override
     protected void runPlan1() {
@@ -29,7 +33,21 @@ public class YourService extends KiboRpcService {
     }
 
     protected void handleTarget() {
-
+        // avoid KOZ
+        Point point = new Point(10.3f, -10.2f, 4.32f);
+        Quaternion quaternion = new Quaternion(0f, 0f, 0f, 1f);
+        api.moveTo(point, quaternion, true);
+        // point 1
+        point = new Point(11.2746d, -9.92284d, 5.2988d);
+        quaternion = new Quaternion(0.0f, 0.0f, -0.707f, 0.707f);
+        api.moveTo(point, quaternion, true);
+        api.laserControl(true);
+        api.takeTargetSnapshot(1);
+        quaternion = new Quaternion(0.707f, 0.0f, 0.0f, 0.707f);
+        api.moveTo(point, quaternion, true);
+        api.laserControl(true);
+        api.takeTargetSnapshot(1);
+        api.reportMissionCompletion("Mission Complete!");
     }
 
 }
