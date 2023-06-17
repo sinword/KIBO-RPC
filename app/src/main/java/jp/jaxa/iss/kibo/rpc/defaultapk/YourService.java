5ae34d4f4a6ec42d92cdf98c35f6fb4b88251d0b
MeowOhMeow <abc22681157@gmail.com>
2023-06-18 00:01:05 +0800

    target goup

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -3,32 +3,33 @@ package jp.jaxa.iss.kibo.rpc.defaultapk;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
 /**
- * Class meant to handle commands from the Ground Data System and execute them in Astrobee
+ * Class meant to handle commands from the Ground Data System and execute them
+ * in Astrobee
  */
 
 public class YourService extends KiboRpcService {
+
     @Override
-    protected void runPlan1(){
+    protected void runPlan1() {
         // write your plan 1 here
     }
 
     @Override
-    protected void runPlan2(){
+    protected void runPlan2() {
         // write your plan 2 here
     }
 
     @Override
-    protected void runPlan3(){
+    protected void runPlan3() {
         // write your plan 3 here
     }
 
-    protected void HandleQRCode(){
+    protected void handleQRCode() {
 
     }
 
-    protected void HandleTarget(){
-        
+    protected void handleTarget() {
+
     }
 
 }
-
