633a131e60b5ab72cf80d89f609af50874b0e1ab
MeowOhMeow <abc22681157@gmail.com>
2023-06-23 08:42:17 +0800

    log data after rotation

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -350,14 +350,13 @@ public class YourService extends KiboRpcService {
         targetPoint.put(1, 0, pos.x + config.NAV_CAM_POSITION[1]);
         targetPoint.put(2, 0, pos.y + config.NAV_CAM_POSITION[2]);
         Log.i(TAG, "Relative to center of kibo in its cords: " + targetPoint.dump());
-
         // (1, 0, 0) represents the direction of the laser which is shoot forward
         Mat lineDirection = new Mat(3, 1, CvType.CV_64FC1);
         lineDirection.put(0, 0, 1);
         lineDirection.put(1, 0, 0);
         lineDirection.put(2, 0, 0);
         Mat linePoint = new Mat(3, 1, CvType.CV_64FC1);
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; ++i) {
             linePoint.put(i, 0, config.LASER_POSITION[i]);
         }
 
@@ -366,10 +365,10 @@ public class YourService extends KiboRpcService {
         double[] dr = lineRotation.getQuaternion();
         Log.i(TAG, "quaternion: " + dr[0] + ", " + dr[1] + ", " + dr[2] + ", " + dr[3]);
 
-        Quaternion originalQuaternion = api.getRobotKinematics().getOrientation();
-        double[] originalQuaternionInDouble = { originalQuaternion.getW(), originalQuaternion.getX(),
-                originalQuaternion.getY(), originalQuaternion.getZ() };
-        double[] totalRotation = multiplyQuaternions(dr, originalQuaternionInDouble);
+        Quaternion originalOrientation = api.getRobotKinematics().getOrientation();
+        double[] originalOrientationInDouble = { originalOrientation.getW(), originalOrientation.getX(),
+                originalOrientation.getY(), originalOrientation.getZ() };
+        double[] totalRotation = multiplyQuaternions(dr, originalOrientationInDouble);
         if (totalRotation == null) {
             Log.i(TAG, "totalRotation is null");
             return;
@@ -389,6 +388,25 @@ public class YourService extends KiboRpcService {
         api.saveMatImage(image, "target_" + targetNumber + "_" + config.count[targetNumber]
                 + ".png");
         config.count[targetNumber]++;
+        corners = new ArrayList<>();
+        ids = new Mat();
+        Aruco.detectMarkers(image, config.arucoDict, corners, ids);
+        rvecs = new Mat();
+        tvecs = new Mat();
+        Aruco.estimatePoseSingleMarkers(corners, config.MARKER_LENGTH, config.navCamMatrix,
+                config.distCoeffs, rvecs, tvecs);
+        estimation = new Estimation(ids, rvecs, tvecs);
+        pos = estimation.getEstimatedPos();
+        Log.i(TAG, "Relative to camera: " + pos.x + ", " + pos.y + ", " + pos.z);
+        targetPoint = new Mat(3, 1, CvType.CV_64FC1);
+        targetPoint.put(0, 0, pos.z + config.NAV_CAM_POSITION[0]);
+        targetPoint.put(1, 0, pos.x + config.NAV_CAM_POSITION[1]);
+        targetPoint.put(2, 0, pos.y + config.NAV_CAM_POSITION[2]);
+        Log.i(TAG, "Relative to center of kibo in its cords: " + targetPoint.dump());
+        for (int i = 0; i < 3; ++i) {
+            targetPoint.put(i, 0, targetPoint.get(i, 0)[0] - config.LASER_POSITION[i]);
+        }
+        Log.i(TAG, "Relative to laser: " + targetPoint.dump());
     }
 
     private void handleTarget(int targetNumber) {
